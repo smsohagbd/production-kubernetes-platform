a2f2630df8eea20f4aff9ee7c2b86ac0414e5941
Sohag Hossain <sohagbdmt@gmail.com>
2026-08-11 18:17:07 +0600

    added backend api to frontend

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Production Backend")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://sohag.com.bd",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/health")
